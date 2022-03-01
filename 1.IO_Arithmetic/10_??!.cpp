b7a4bf38c512a4b0d35d624674c79fdd8578e45b
KangHyeon3315 <son2786w@gmail.com>
2022-03-01 14:19:38 +0900

    입출력과 사칙연산

---
+++ 1.IO_Arithmetic/10_??!.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    string input_str;
+    getline(cin, input_str);
+
+    cout << input_str << "\?\?!" << endl;
+    
+    return 0;
+}
---
+++ 1.IO_Arithmetic/2_we_love_kriii.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string output = "강한친구 대한육군\n";
+    cout << output << output;
+    return  0;
+}
